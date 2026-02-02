1d013f4863577e95736e2433d7493341d8935dd5
bmolist <bielmolistimas@gmail.com>
2026-02-02 12:00:26 +0100

    desenvolupar codi del Guerrer

--- src/Guerrer.java
+++ src/Guerrer.java
@@ -1,3 +1,22 @@
-public class Guerrer {
+public class Guerrer extends ClasseBaseEntitat {
+    private int resistencia;
 
+    public Guerrer(String nom, int nivell, int puntsVida, int resistencia) {
+        super(nom, nivell, puntsVida);
+        this.resistencia = resistencia;
+    }
+
+    public int getResistencia() { return resistencia; }
+    public void setResistencia(int r) { this.resistencia = r; }
+
+    public void atacar() {
+        System.out.println(getNom() + " fot un cop d'espasa!");
+    }
+
+    public void rebreDany(int quantitat) {
+        // Restem la resistencia al dany
+        int danyReal = quantitat - resistencia;
+        if (danyReal < 0) danyReal = 0;
+        super.rebreDany(danyReal);
+    }
 }
