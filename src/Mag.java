5e0291d60e7759bf564bf1dadee08bfaf7b58b93
bmolist <bielmolistimas@gmail.com>
2026-02-02 12:08:13 +0100

    desenvolupar codi del Mag

--- src/Mag.java
+++ src/Mag.java
@@ -1,2 +1,15 @@
-public class Mag {
+public class Mag extends ClasseBaseEntitat {
+    private int mana;
+
+    public Mag(String nom, int nivell, int puntsVida, int mana) {
+        super(nom, nivell, puntsVida);
+        this.mana = mana;
+    }
+
+    public int getMana() { return mana; }
+    public void setMana(int m) { this.mana = m; }
+
+    public void atacar() {
+        System.out.println(getNom() + " llança una bola de foc!");
+    }
 }
