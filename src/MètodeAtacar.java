ad537402b846b47e009b2fcd75e7a085252b874c
bmolist <bielmolistimas@gmail.com>
2026-02-06 13:11:05 +0100

    montar classe MetodeAtacar

--- src/MètodeAtacar.java
+++ src/MètodeAtacar.java
@@ -1,7 +1,6 @@
 public class MètodeAtacar {
-    // Simulació de cartes especials (Fase 4)
-    public void cartaTrenMalalt(ClasseBaseEntitat objectiu) {
+    public void usarCartaEspecial(ClasseBaseEntitat objectiu) {
         System.out.println("S'activa la carta: Atac del tren malalt!");
-        objectiu.rebreDany(5); // Resta -5 de vida
+        objectiu.rebreDany(5);
     }
 }
