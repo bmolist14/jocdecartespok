96cd90b007a515118552afe14a1676dfaa32a7ed
bmolist <bielmolistimas@gmail.com>
2026-02-06 13:44:47 +0100

    atac

--- src/MètodeAtacar.java
+++ src/MètodeAtacar.java
@@ -1,6 +1,6 @@
 public class MètodeAtacar {
     public void usarCartaEspecial(ClasseBaseEntitat objectiu) {
-        System.out.println("S'activa la carta: Atac del tren malalt!");
+        System.out.println("S'activa la carta: Atac ");
         objectiu.rebreDany(5);
     }
 }
